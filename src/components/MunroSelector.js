a299d3adf63bba7df44c96c4b7559469b7eeddfa
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-13 13:53:35 +0100

    3rd commit main

--- src/components/MunroSelector.js
+++ src/components/MunroSelector.js
@@ -2,12 +2,17 @@
 import React from "react";
 
 const MunroSelector = ({munros, onMunroSelected} => {
+    
+    const handleChange = function(event){
+        onMunroSelected(munros[event.target.value]);
+    }
 
     const munroOptions = munros.map((munro, index) => {
         return <option value={index} key={index}>{munro.name}</option>
     })
+
     return(
-        <select>
+        <select onChange={handleChange}>
             {munroOptions}
         </select>
     )
